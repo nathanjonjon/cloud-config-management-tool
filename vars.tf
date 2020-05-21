8aa78ef3c7976954785b3731ecf85bcf39ee07e7
nathanjonjon <b05902112@ntu.edu.tw>
2020-05-21 15:48:24 +0800

    test

--- vars.tf
+++ vars.tf
@@ -2,12 +2,12 @@ variable "AWS_REGION" {
   default = "us-east-1"
 }
 
-variable "aws_key_pair" {}
+# variable "aws_key_pair" {}
 
 variable "availability_zone" {
   default = "us-east-1a"
 }
-variable "key_name" {}
+# variable "key_name" {}
 
-variable "access_key" {}
-variable "secret_key" {}
+# variable "access_key" {}
+# variable "secret_key" {}
