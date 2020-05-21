45cdcfa3a448859409ca4b4e2d2a3ae37500f425
nathanjonjon <b05902112@ntu.edu.tw>
2020-05-21 11:02:11 +0800

    edit the setting of credentials

---
+++ vars.tf
@@ -0,0 +1,13 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "aws_key_pair" {}
+
+variable "availability_zone" {
+  default = "us-east-1a"
+}
+variable "key_name" {}
+
+variable "access_key" {}
+variable "secret_key" {}
