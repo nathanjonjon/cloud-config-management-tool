c6845e27feb948f667ee4a230ae13ef1a1a50c26
nathanjonjon <b05902112@ntu.edu.tw>
2020-05-25 14:50:25 +0800

    update sg

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["10.0.0.0/16", "220.133.223.215/32", "211.75.165.158/32"]
   }
 
   egress {
@@ -22,7 +22,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["10.0.0.0/16", "220.133.223.215/32", "211.75.165.158/32"]
   }
 
   egress {
@@ -92,10 +92,6 @@ resource "aws_instance" "Nathan_WebServer" {
 }
 
 resource "aws_instance" "Nathan_DB" {
-<<<<<<< HEAD
-  # ami             = "ami-003634241a8fcdec0"
-=======
->>>>>>> master
   ami = data.aws_ami.ubuntu.id # Ubuntu Server  latest version
   instance_type   = "t2.micro"
   availability_zone = var.availability_zone
@@ -103,14 +99,4 @@ resource "aws_instance" "Nathan_DB" {
   vpc_security_group_ids = [aws_security_group.db_sg.id]
   subnet_id = aws_subnet.main-private.id
   tags = {Name = "Nathan_DB", Owner = "Nathan"}
-<<<<<<< HEAD
-  # connection { # Connect remote EC2
-  #   type        = "ssh"
-  #   host        = self.public_ip # bind public ip
-  #   user        = "ec2-user"
-  #   private_key = file(var.aws_key_pair)
-  # }
 }
-=======
-}
->>>>>>> master
