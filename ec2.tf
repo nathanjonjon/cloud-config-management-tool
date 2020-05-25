de3415ed8f103308c1ee164a21c9e9b4dad31a1a
nathanjonjon <b05902112@ntu.edu.tw>
2020-05-25 10:49:15 +0800

    edit .yml main.tf ec2.tf vpc.tf

---
+++ ec2.tf
@@ -0,0 +1,94 @@
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "80 port"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "22 port"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {Name = "Nathan_WebServer_sg", Owner = "Nathan"}
+  
+}
+
+resource "aws_security_group" "db_sg" {
+  name        = "db_sg"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "80 port"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.web_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "22 port"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    security_groups = [aws_security_group.web_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {Name = "Nathan_DB_sg", Owner = "Nathan"}
+  
+}
+
+
+
+resource "aws_instance" "Nathan_WebServer" {
+  # ami             = "ami-003634241a8fcdec0"
+  ami = data.aws_ami.ubuntu.id # Ubuntu Server  latest version
+  instance_type   = "t2.micro"
+  availability_zone = var.availability_zone
+  key_name = var.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  subnet_id = aws_subnet.main-public.id
+  tags = {Name = "Nathan_WebServer", Owner = "Nathan"}
+  # connection { # Connect remote EC2
+  #   type        = "ssh"
+  #   host        = self.public_ip # bind public ip
+  #   user        = "ec2-user"
+  #   private_key = file(var.aws_key_pair)
+  # }
+}
