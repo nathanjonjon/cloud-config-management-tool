afc838c0ad12b5f2162a40b78c5f4b6955fef1ea
LYTzeng <oscar217b@gmail.com>
2020-06-01 23:19:28 +0800

    Modify webhook listener config and script

--- webhook_listener/webhook_script.sh
+++ webhook_listener/webhook_script.sh
@@ -1,25 +1,20 @@
 #!/bin/sh
-echo $1
-echo $2
+echo "Repo name is $1"
+echo "Github Project owner is $2"
+echo $type
 
 project_id=$(curl -XGET \
     -u admin:password \
     "http://0.0.0.0/api/v2/projects/?name=$1" 2>/dev/null | jq -r '.results[0].id')
 
-# project_id=$(curl -XGET \
-#     -u admin:password \
-#     "http://0.0.0.0/api/v2/projects/?name=awx_webhook" 2>/dev/null | jq -r '.results[0].id')
-
 template_id=$(curl -XGET \
     -u admin:password \
-    "http://0.0.0.0/api/v2/job_templates/?project=$project_id&name=plan_awx_webhook" 2>/dev/null | jq -r '.results[0].id')
+    "http://0.0.0.0/api/v2/job_templates/?project=$project_id&name=plan_nextlinker_cmdb" 2>/dev/null | jq -r '.results[0].id')
 
 echo $project_id
 echo $template_id
 
-curl -XPOST -H "Content-Type: application/json" \
-    --data "{}" \
+curl -qsOL -XPOST -H "Content-type: application/json" \
+    --data '{}' \
     -u admin:password  \
     http://0.0.0.0/api/v2/job_templates/$template_id/launch
-
-#curl -XGET -u admin:password "http://0.0.0.0/api/v2/job_templates/?project=8"
