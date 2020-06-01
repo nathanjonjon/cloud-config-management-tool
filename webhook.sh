0b22d3abb18c9d3153d9e8ee837576178c7c16d4
nathanjonjon <nathan.chang@nextlink.com.tw>
2020-06-01 11:40:35 +0800

    naughty

---
+++ webhook.sh
@@ -0,0 +1,3 @@
+echo i am handsome
+curl -i -X POST -H 'Content-Type: application/json' -d '{"text": "Nextlinker Assemble!"}' https://mattermost.nextlink.technology/hooks/fea4je9g6tnw98c7z5phj7ofyh
+echo you are handsome too
