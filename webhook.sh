3968576a97fa3ad181356f0b3fce4846ec02a16e
nathanjonjon <nathan.chang@nextlink.com.tw>
2020-06-01 12:07:26 +0800

    test webhook

--- webhook.sh
+++ webhook.sh
@@ -1,2 +1,12 @@
+if [ ! -n $2 ] ; then
+    echo "Usage: webhook.sh <url> <branch>"
+    exit 1;
+fi
+
+mattermost_url="$1"
+branch="$2"
+
+# Specify the development branch and stable branch names
+
 echo i am handsome
-curl -i -X POST -H 'Content-Type: application/json' -d '{"text": "Nextlinker Assemble!\n message from $TRAVIS_BRANCH"}' $mattermost_url
+curl -i -X POST -H 'Content-Type: application/json' -d '{"text": "Nextlinker Assemble!\n message from "  + "$branch"}' $mattermost_url
