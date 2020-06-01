c56ed5eca4e8941c271babe702e08845e75c5148
nathanjonjon <nathan.chang@nextlink.com.tw>
2020-06-01 11:59:19 +0800

    add msg

--- webhook.sh
+++ webhook.sh
@@ -1,3 +1,2 @@
 echo i am handsome
-curl -i -X POST -H 'Content-Type: application/json' -d '{"text": "Nextlinker Assemble!"}' $mattermost_url
-echo you are handsome too
+curl -i -X POST -H 'Content-Type: application/json' -d '{"text": "Nextlinker Assemble!\n message from $TRAVIS_BRANCH"}' $mattermost_url
