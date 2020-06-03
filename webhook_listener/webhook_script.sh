31b31aac9b41a3b4c096dfd813def9bac30c0124
LYTzeng <oscar217b@gmail.com>
2020-06-03 17:09:26 +0800

    Use main.yml only in AWX by configuring variables in job templates

--- webhook_listener/webhook_script.sh
+++ webhook_listener/webhook_script.sh
@@ -11,10 +11,14 @@ template_id=$(curl -XGET \
     -u admin:password \
     "http://0.0.0.0/api/v2/job_templates/?project=$project_id&name=deploy nextlinker_cmdb" 2>/dev/null | jq -r '.results[0].id')
 
+workflow_id=$(curl -XGET \
+    -u admin:password \
+    "http://0.0.0.0/api/v2/workflow_job_templates/?name=nextlinker_cmdb workflow" 2>/dev/null | jq -r '.results[0].id')
+
 echo $project_id
 echo $template_id
 
 curl -qsOL -XPOST -H "Content-type: application/json" \
     --data '{}' \
     -u admin:password  \
-    http://0.0.0.0/api/v2/job_templates/$template_id/launch
+    http://0.0.0.0/api/v2/workflow_job_templates/$workflow_id/launch
