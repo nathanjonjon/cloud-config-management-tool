43fbfc3c2c576236ecc2758fff365e3fe8673d83
internnn <65592584+internnn@users.noreply.github.com>
2020-06-22 17:09:33 +0800

    Delete db.php

--- db.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$connection = new PDO('mysql:host=localhost;dbname=Pass', 'Nextlink', '1234');
-$statement  = $connection->query('SELECT message FROM Pass');
-
-echo $statement->fetchColumn(); 
